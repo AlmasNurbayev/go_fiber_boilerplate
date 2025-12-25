133355cffe4799e9188e3269f0eed80b9729ef14
almas <ntldrzic@gmail.com>
2025-12-25 15:33:10 +0500

    Refactor user model and DTO to standardize null handling for phone number and email fields

--- internal/httpApp/dto/users.go
+++ internal/httpApp/dto/users.go
@@ -1,8 +1,6 @@
 package dto
 
-import (
-	"github.com/guregu/null/v6"
-)
+import "github.com/guregu/null/v6"
 
 type UserRequestParams struct {
 	Id int64 `validate:"required,gte=0" example:"5"`
@@ -14,8 +12,8 @@ type UserRequestQueryParams struct {
 
 type UserResponse struct {
 	Id           int64       `json:"id"`
-	Phone_number string      `json:"phone_number"`
-	Email        string      `json:"email"`
-	Name         null.String `json:"name"`
-	Role_name    null.String `json:"role_name"`
+	Phone_number null.String `json:"phone_number"`
+	Email        null.String `json:"email"`
+	Name         string      `json:"name"`
+	Role_name    string      `json:"role_name"`
 }
--- internal/models/users.go
+++ internal/models/users.go
@@ -8,11 +8,11 @@ import (
 
 type UserEntity struct {
 	Id            int64       `json:"id" db:"id"`
-	Phone_number  string      `json:"phone_number" db:"phone_number"`
-	Email         string      `json:"email" db:"email"`
-	Name          null.String `json:"name" db:"name"`
-	Password_hash null.String `json:"password_hash" db:"password_hash"`
-	Role_id       null.Int64  `json:"role" db:"role_id"`
+	Phone_number  null.String `json:"phone_number" db:"phone_number"`
+	Email         null.String `json:"email" db:"email"`
+	Name          string      `json:"name" db:"name"`
+	Password_hash string      `json:"password_hash" db:"password_hash"`
+	Role_id       int64       `json:"role" db:"role_id"`
 	Changed_date  time.Time   `json:"changed_date" db:"changed_date"`
 	Create_date   time.Time   `json:"create_date" db:"create_date"`
 }
